6b70bfa8e5b45a9cb259a20bc33c918a36f8ac1d
Ronney <ronneyrv@gmail.com>
2026-05-21 19:59:05 -0300

    fix: correct swiper card layout

--- src/components/ProjectCarousel/ProjectCarousel.tsx
+++ src/components/ProjectCarousel/ProjectCarousel.tsx
@@ -1,6 +1,12 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import type { Project } from "../../types/Projects";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
 import ProjectCard from "../ProjectCard/ProjectCard";
+
+import type { Project } from "../../types/Projects";
 
 interface Props {
   projects: Project[];
@@ -9,15 +15,31 @@ interface Props {
 export default function ProjectCarousel({ projects }: Props) {
   return (
     <Swiper
-      spaceBetween={20}
+      modules={[Navigation]}
+      navigation
+      className="w-full"
+      spaceBetween={30}
       slidesPerView={1}
       breakpoints={{
-        768: { slidesPerView: 2 },
-        1024: { slidesPerView: 3 },
+        640: {
+          slidesPerView: 1,
+        },
+
+        768: {
+          slidesPerView: 2,
+        },
+
+        1024: {
+          slidesPerView: 3,
+        },
+
+        1440: {
+          slidesPerView: 4,
+        },
       }}
     >
-      {projects.map((project) => (
-        <SwiperSlide key={project.id}>
+      {projects.map(project => (
+        <SwiperSlide key={project.id} className="flex h-auto">
           <ProjectCard project={project} />
         </SwiperSlide>
       ))}
